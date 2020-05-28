9a78d7d909ef70d922af35f9e3af1f29319ff71b
Tim Wagner <tim.wagner@ibykus.de>
2020-05-28 13:18:12 +0200

    - Methode hinzugefügt, die die Daten der gesamten Tabelle zurückgibt - durch die Verwendung von "try with resources" wird die Verbindung beim Verlassen des Catch-Blocks automatisch geschlossen

--- src/client/DBReader.java
+++ src/client/DBReader.java
@@ -1,4 +1,17 @@
 package client;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBReader {
+    private String url;
+
+    public void getAllData() {
+        try (Connection con = DriverManager.getConnection(url)) {
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
