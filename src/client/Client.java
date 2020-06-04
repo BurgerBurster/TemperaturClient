69d3918188ba1f8ec9bafec91fe24d033d9ec628
MarcusCoding <marcus_voigt@gmx.net>
2020-06-04 11:05:55 +0200

    [+] pulic variables for temp and sensor list [~] connection to mysql

--- src/client/Client.java
+++ src/client/Client.java
@@ -3,7 +3,10 @@ package client;
 public class Client {
     public static void main(String[] args) {
         System.out.println("Hallo Welt");
-        DBReader reader = new DBReader("jdbc:mysql://mysql04.manitu.net/db25461?serverTimezone=UTC&verifyServerCertificate=false");
+        DBReader reader = new DBReader("jdbc:mysql://mysql04.manitu.net/db25461?serverTimezone=UTC&verifyServerCertificate=false&autoReconnect=true&useSSL=false");
+
         reader.getAllData();
+
+
     }
 }
--- src/client/DBReader.java
+++ src/client/DBReader.java
@@ -18,41 +18,44 @@ public class DBReader {
         this.url = dbUrl;
     }
 
+    public List<TemperaturModel> listTemps;
+    public List<SensorModel> listSensor;
+
     public void getAllData() {
         try (Connection con = DriverManager.getConnection(url, "u25461", "wk3bwBmShWv5")) {
 
             //Temperatur Daten
-            List<TemperaturModel> listTemps = new ArrayList<>();
+            listTemps = new ArrayList<>();
 
             Statement stmt = con.createStatement();
             ResultSet resultSet = stmt.executeQuery("SELECT * FROM temperaturen");
 
             while(resultSet.next()){
                 TemperaturModel item = new TemperaturModel();
-                item.TemperaturID = Integer.parseInt(resultSet.getString(0));
-                item.sensorNr = Integer.parseInt(resultSet.getString(1));
-                item.Zeit = resultSet.getString(2);
-                item.Temperatur = Double.parseDouble(resultSet.getString(3));
-                item.datum = resultSet.getString(4);
+                item.TemperaturID = Integer.parseInt(resultSet.getString(1));
+                item.sensorNr = Integer.parseInt(resultSet.getString(2));
+                item.Zeit = resultSet.getString(3);
+                item.Temperatur = Double.parseDouble(resultSet.getString(4));
+                item.datum = resultSet.getString(5);
                 listTemps.add(item);
             }
             stmt.close();
 
             //Sensor Daten
-            List<SensorModel> listSensor = new ArrayList<>();
+            listSensor = new ArrayList<>();
 
             Statement stmtSensor = con.createStatement();
-            ResultSet resultSetSensor = stmtSensor.executeQuery("SELECT * FROM sensoren");
+            ResultSet resultSetSensor = stmtSensor.executeQuery("SELECT sensorNr, serverschrank, adresse, herstellerNr, maxtemperatur FROM sensoren");
 
             while(resultSetSensor.next()){
                 SensorModel item = new SensorModel();
-                item.sensorNr = Integer.parseInt(resultSetSensor.getString(0));
-                item.serverschrank = Integer.parseInt(resultSetSensor.getString(1));
-                item.adresse = resultSetSensor.getString(2);
-                item.herstellerNr = Integer.parseInt(resultSetSensor.getString(3));
-                item.maxTemperatur = Double.parseDouble(resultSetSensor.getString(4));
+                item.sensorNr = Integer.parseInt(resultSetSensor.getString(1));
+                item.serverschrank = Integer.parseInt(resultSetSensor.getString(2));
+                item.adresse = resultSetSensor.getString(3);
+                item.herstellerNr = Integer.parseInt(resultSetSensor.getString(4));
+                item.maxTemperatur = Double.parseDouble(resultSetSensor.getString(5));
 
-                System.out.println(resultSetSensor.getString(0));
+                System.out.println(resultSetSensor.getString(2));
 
                 listSensor.add(item);
             }
