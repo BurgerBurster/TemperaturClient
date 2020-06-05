9596e604f5ddbda6f5a055c0fcef77915a987995
Tim Wagner <tim.wagner@ibykus.de>
2020-06-05 11:32:05 +0200

    - gui branch wurde per merge auf dem master gebracht - Aufruf der GUI von Lucas in der Client-Klasse

--- src/client/Client.java
+++ src/client/Client.java
@@ -4,5 +4,7 @@ public class Client {
     public static void main(String[] args) {
         DBReader reader = new DBReader("jdbc:mysql://mysql04.manitu.net/db25461?serverTimezone=UTC&verifyServerCertificate=false&autoReconnect=true&useSSL=false");
         reader.getAllData();
+
+        GUI.callGui();
     }
 }
